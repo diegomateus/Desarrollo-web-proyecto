f777d7e48de0e5d735ab9e6c822ac501b2669bc5
Reshi01 <danieldanih97@gmail.com>
2021-05-03 12:28:17 -0500

    Tabla de propiedad creada en BD
    No se ha probado la base de datos

--- Creacion_de_Tablas.sql
+++ Creacion_de_Tablas.sql
@@ -18,3 +18,23 @@ CREATE TABLE `appointments` (
   `customer_phone_number` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `real_estate`.`properties` (
+  `id` VARCHAR(36) NOT NULL,
+  `address` VARCHAR(50) NOT NULL,
+  `property_type` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(85) NOT NULL,
+  `description` VARCHAR(500) NOT NULL,
+  `bedrooms_number` INT NOT NULL,
+  `bathrooms_number` INT NOT NULL,
+  `private_area` INT NOT NULL,
+  `built_area` INT NOT NULL,
+  `service_level` INT NOT NULL,
+  `condition` VARCHAR(45) NOT NULL,
+  `delivery_date` DATE NOT NULL,
+  `latitude` BIGINT NOT NULL,
+  `longitude` BIGINT NOT NULL,
+  `images` VARCHAR(1000) NULL DEFAULT NULL,
+  `offers` VARCHAR(1000) NULL DEFAULT NULL,
+  `questions` VARCHAR(1000) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`));
