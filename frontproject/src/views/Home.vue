4eb2e9221baa0f0c623a56688e106fa4cfad895d
SantiagoCaroprese <66383253+SantiagoCaroprese@users.noreply.github.com>
2021-06-04 17:39:26 -0500

    Update Home.vue

--- frontproject/src/views/Home.vue
+++ frontproject/src/views/Home.vue
@@ -11,14 +11,11 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-//import InitialSearch from "@/components/search/InitialSearch.vue";
 import initialSearch from "@/components/search/InitialSearch.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
     initialSearch,
   },
 });
